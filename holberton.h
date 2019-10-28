47f9770543826b75532d0b202feb44ce1503fd54
MichaelAuditore <michael_v613@hotmail.com>
2019-10-28 16:56:26 +0000

    [Feature]: New prototypes

--- holberton.h
+++ holberton.h
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <stddef.h>
+#include <unistd.h>
 /**
  * struct vaargs - struct to canalize buffer
  * @typef: Type of format
@@ -13,6 +14,7 @@ typedef struct vaargs
 	char typef;
 	int (*f)();
 } vargs;
+int _printf(const char *format, ...);
 int _putchar(char c);
 void copyto_buffer(char *dest, char letter, int *pos);
 int (*looktype(char s))(va_list args, char *dest, int *pos);
@@ -20,5 +22,6 @@ int save_string(va_list args, char *dest, int *pos);
 int save_char(va_list args, char *dest, int *pos);
 int save_reverse(va_list args, char *dest, int *pos);
 int save_decimal(va_list args, char *dest, int *pos);
+int save_percent(va_list args, char *dest, int *pos);
 void convert_numbers(char *dest, unsigned int number, int *pos);
 #endif
