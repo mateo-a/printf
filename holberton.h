730abd78d20c2fd5f6d5539ba59bc2f1e47f67b4
mateo-a <alexander.urrego@gmail.com>
2019-10-29 21:37:56 +0000

    [Feature]: Add funtion to option 'S'

--- holberton.h
+++ holberton.h
@@ -21,7 +21,7 @@ void copyto_buffer(char *dest, char letter, int *pos);
 int (*looktype(char s))(va_list args, char *dest, int *pos);
 int save_string(va_list args, char *dest, int *pos);
 int save_char(va_list args, char *dest, int *pos);
-int save_reverse(va_list args, char *dest, int *pos);
+int save_upstring(va_list args, char *dest, int *pos);
 void rev_string(char *s);
 int save_number(va_list args, char *dest, int *pos);
 int save_percent(va_list args, char *dest, int *pos);
