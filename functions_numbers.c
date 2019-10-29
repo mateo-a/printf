7202b923f285d94b615141e64da534722d7d5023
MichaelAuditore <michael_v613@hotmail.com>
2019-10-29 16:46:15 +0000

    [Fix]: Output for numbers

--- functions_numbers.c
+++ functions_numbers.c
@@ -1,48 +1,39 @@
 #include "holberton.h"
 /**
- * convert_numbers - convert a number into char and copy into buffer
- * @dest: buffer destiny
- * @number: number to convert a char
- * @pos: Last position of buffer
+ * save_number - save a number
+ * @args: List of arguments
+ * @dest: Destiny to copy
+ * @pos: Position of destiny
+ * Return: The length
  */
-void convert_numbers(char *dest, int number, int *pos)
+int save_number(va_list args, char *dest, int *pos)
 {
-	char cnumber;
+	int n, div, len;
+	unsigned int num;
 
-	if (number / 10 != 0)
-	{
-		convert_numbers(dest, number / 10, pos);
-	}
-	cnumber = (number % 10) + '0';
-	copyto_buffer(dest, cnumber, pos);
-}
+	n  = va_arg(args, int);
+	div = 1;
+	len = 0;
 
-/**
- * save_decimal - validate a number
- * @args: list of arguments to validate
- * @dest: buffer destiny
- * @pos: Last position of buffer
- * Return: Actual length
- */
-int save_decimal(va_list args, char *dest, int *pos)
-{
-	int number = va_arg(args, int);
-	if (number < 0)
+	if (n < 0)
 	{
+		len++;
 		copyto_buffer(dest, '-', pos);
-		number = number * -1;
+		num = n * -1;
 	}
-	convert_numbers(dest, number, pos);
-	return (strlen_numbers(number));
-}
-/**
- * strlen_numbers - gets the length of a number
- * @number: Number to knows the length
- * Return: Length of numbers
- */
-int strlen_numbers(int number)
-{
-	if (number > 0)
-		return (strlen_numbers(number / 10) + 1);
-	return (0);
+	else
+		num = n;
+
+	for (; num / div > 9; )
+		div *= 10;
+
+	for (; div != 0; )
+	{
+		len++;
+		copyto_buffer(dest, ('0' + num / div), pos);
+		num %= div;
+		div /= 10;
+	}
+
+	return (len);
 }
